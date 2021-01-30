fdd13afcc86734d50417f3ffa8cc9893110564e8
Caio <caiookb@gmail.com>
2021-01-30 01:38:33 -0300

    feature(Utils): Created utils file to help testing and some extra functions, created Color file to manage colors quickly

---
+++ src/utils/Colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: "#116193",
+  secondary: "#00e8e4",
+  third: "#1f6f9c",
+  fourth: "#84aac2",
+  background: "#e6e6e6",
+  lightBackground: "#f2f2f2",
+  white: "#FFF",
+  text: "#7a7a7a",
+};
