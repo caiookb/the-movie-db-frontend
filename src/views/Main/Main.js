d87ab0e38e4f78805ad88c3a1f1aa48f11553c2c
Caio <caiookb@gmail.com>
2021-01-31 05:10:13 -0300

    feature(Main): Debouncing search query

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,38 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Card, Header, Pagination, TextInput } from "../../components";
 import { MoviesController } from "../../controller";
 import { StyledMain } from "./styles";
+import { debounce } from "lodash";
 
 const Main = (props) => {
-  const { searchMovies, moviesList } = props;
+  const {
+    searchMovies,
+    selectMovie,
+    currentPageMovies,
+    moviesList,
+    paginateItems,
+    history,
+  } = props;
+
+  const handleChange = useCallback(
+    debounce((text) => {
+      searchMovies(text);
+    }, 1000),
+    []
+  );
+
+  const handleSelectMovie = (movie) => {
+    selectMovie(movie);
+    history.push(`/movie/${movie.id}`);
+  };
 
   return (
     <StyledMain>
       <Header title={"Movies"} />
       <TextInput
         placeholder={"Busque um filme por nome, ano ou gênero..."}
-        onChange={searchMovies}
+        onChange={(e) => handleChange(e.target.value)}
       />
-      {moviesList?.map((movie) => (
-        <Card movie={movie} />
+      {currentPageMovies?.map((movie, key) => (
+        <Card
+          onClick={() => handleSelectMovie(movie)}
+          movie={movie}
+          key={key}
+        />
       ))}
-
-      <Pagination list={moviesList} />
+      <Pagination list={moviesList} paginateItems={paginateItems} />
     </StyledMain>
   );
 };
 
 const mapStateToProps = (state) => {
   const {
-    movies: { moviesList },
+    movies: { moviesList, currentPage },
   } = state;
-  return { moviesList };
+  return { moviesList, currentPageMovies: currentPage };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     searchMovies: (name) => dispatch(MoviesController.searchMovie(name)),
+    selectMovie: (movie) => dispatch(MoviesController.selectedMovieCard(movie)),
+    paginateItems: (list) => dispatch(MoviesController.paginateMovies(list)),
   };
 };
 
