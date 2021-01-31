e7d4d31ee75c42fec9c7bcebd69f757eeb50fad1
Caio <caiookb@gmail.com>
2021-01-31 12:36:09 -0300

    feature(Details): fetching movie by id

--- src/views/Details/Details.js
+++ src/views/Details/Details.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { DetailedCard, Header } from "../../components";
+import { MoviesController } from "../../controller";
 import { StyledDetails } from "./Styles";
 
 const Details = (props) => {
-  const { selectedMovie } = props;
+  const { selectedMovie, fetchById } = props;
 
-  console.log("SEELC", selectedMovie);
+  const splitUrl = window.location.href.split("/");
+  const movieId = { id: splitUrl[splitUrl.length - 1] };
+
+  useEffect(() => {
+    fetchById(movieId);
+  }, []);
 
   return (
     <StyledDetails>
@@ -23,4 +29,10 @@ const mapStateToProps = (state) => {
   return { selectedMovie };
 };
 
-export default connect(mapStateToProps, null)(Details);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchById: (id) => dispatch(MoviesController.selectedMovieCard(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Details);
